b3f38503ed27217680d5c28b3e02829636af0c8e
simbankenya <pharesmuruthi@gmail.com>
2023-05-17 08:27:54 +0300

    handled validation of client, and validators for client, project and events

--- routes/Clients.js
+++ routes/Clients.js
@@ -2,8 +2,10 @@
  const router = express.Router();
  const _ = require('lodash')
  const { Client } = require('../models/Client');
+const { validateClient } = require('../utils');
  
 
+//fectch all clients
  router.get('/', async (req, res) => {
     const clientList = await Client.find();
     if (!clientList) return res.status(400).send('clients couldnt be fetched');
@@ -12,14 +14,37 @@
  });
 
  router.post('/', async (req, res) => {
+    const { error } = validateClient(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
+    
     let clients = new Client(req.body, _.pick(['clientName', 'hourlyRate', 'email', 'address' ]));
     clients = await clients.save();
     res.send(clients)
     
  });
 
- router.delete('/:id', async (req, res) => {
+ //update clients
+ router.put('/:id', async (req, res) => {
+    const { error } = validateClient(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
 
+    let client = await Client.findByIdAndUpdate(req.params.id, {
+      clientName: req.body.clientName,
+      hourlyRate: req.body.hourlyRate,
+      email: req.body.email, 
+      address: req.body.address
+    }, { new: true});
+
+    res.send(client)
+    
+ });
+
+ //delete a client
+ router.delete('/:id', async (req, res) => {
+   const client = await Client.findByIdAndRemove(req.params.id);
+   if(!client) return res.status(200).send('the client with given id couldnt be deleted')
+
+   res.send(client)
  });
 
 
--- utils.js
+++ utils.js
@@ -10,4 +10,37 @@ const Joi = require('joi')
     return schema.validate(employee)
 }
 
+const validateEvent = (event) => {
+    const schema = Joi.object({
+        duration: Joi.string().required(),
+        eventName : Joi.string().max(40).required(),
+        location: Joi.string().max(40).min(2).required(), 
+        eventDate: Joi.date().required(),
+        eventTime: Joi.string().max(20).required()
+    })
+
+    return schema.validate(event)
+}
+const validateClient = (client) => {
+    const schema = Joi.object({
+        clientName: Joi.string().required().min(2).max(30),
+        email: Joi.string().required().email().min(2).max(30),
+        address: Joi.string().required().min(2).max(50),
+        hourlyRate: Joi.number().required().min(0).max(200)
+    })
+    return schema.validate(client)
+}
+const validateProject = (project) => {
+    const schema = Joi.object({
+        projectName: Joi.string().required().min(2).max(30),
+        client: Joi.objectId().required(),
+        hours: Joi.number().required().min(0).max(200),
+        dateDue : Joi.string().required().min(2).max(30)
+    })
+
+    return schema.validate(project)
+}
 exports.validateEmployee = validateEmployee;
+exports.validateClient = validateClient;
+exports.validateEvent = validateEvent;
+exports.validateProject = validateProject;
